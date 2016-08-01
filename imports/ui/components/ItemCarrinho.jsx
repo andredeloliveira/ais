c0566c6f3935ff31e2c01ccc529d06250dfde3ed
andredeloliveira <oliveira.andrede@gmail.com>
2016-08-01 00:26:35 -0300

    Add total to shoppingcart

--- imports/ui/components/ItemCarrinho.jsx
+++ imports/ui/components/ItemCarrinho.jsx
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 
 export default class ItemCarrinho extends Component {
+  componentWillMount() {
+    this.setState({
+      total: 1 * this.props.preco
+    });
+  }
+
+  getTotal() {
+    return this.state.total || this.props.preco
+  }
+
   setTotal(preco, event) {
     console.log(preco);
      this.setState({
@@ -18,8 +28,8 @@ export default class ItemCarrinho extends Component {
       <tr>
         <td>{produto}</td>
         <td>{preco}</td>
-        <td> <input type="number" name="quantidade" defaultValue="0" style={inputStyle} onChange={this.setTotal.bind(this, preco)}/></td>
-        <td></td>
+        <td> <input type="number" name="quantidade" defaultValue="1" style={inputStyle} onChange={this.setTotal.bind(this, preco)}/></td>
+        <td>{this.getTotal()}</td>
       </tr>
     );
   }

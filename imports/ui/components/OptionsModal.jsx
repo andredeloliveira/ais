23a0fc8106c946479901c191d9f8ccbe7cd6550d
andredeloliveira <oliveira.andrede@gmail.com>
2016-08-01 15:00:53 -0300

    Add pagseguro node API package reference

--- imports/ui/components/OptionsModal.jsx
+++ imports/ui/components/OptionsModal.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { Meteor } from 'meteor/meteor';
 import Login from './Login';
 import Signup from './SignUp';
+import Checkout from './Checkout';
 
 export default class OptionsModal extends Component {
 
@@ -9,26 +11,32 @@ export default class OptionsModal extends Component {
       width: "100%",
       height: "100%"
     };
-    return (
-      <div id="optionsModal" className="modal" style={modalStyle}>
-        <div className="modal-content">
-          <div className="row">
-            <div className="col s12 m6">
-              <div className="card-panel white">
-                <Login/>
+    let currentUser = Meteor.userId();
+    if (currentUser) {
+      return (
+        <Checkout/>
+      )
+    } else
+      return (
+        <div id="optionsModal" className="modal" style={modalStyle}>
+          <div className="modal-content">
+            <div className="row">
+              <div className="col s12 m6">
+                <div className="card-panel white">
+                  <Login/>
+                </div>
+              </div>
+              <div className="col s12 m6">
+                <div className="card-panel white">
+                  <Signup/>
+                </div>
               </div>
             </div>
-            <div className="col s12 m6">
-              <div className="card-panel white">
-                <Signup/>
-              </div>
-            </div>
-            </div>
+          </div>
+          <div className="modal-footer">
+            <a href="#!" className=" modal-action modal-close waves-effect waves-green btn left">Fechar</a>
+          </div>
         </div>
-        <div className="modal-footer">
-          <a href="#!" className=" modal-action modal-close waves-effect waves-green btn left">Fechar</a>
-        </div>
-      </div>
-    );
+      );
   }
 }

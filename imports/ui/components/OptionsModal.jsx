77d4b08957a9f5742cede395bd99af4eeac82304
andredeloliveira <oliveira.andrede@gmail.com>
2016-08-02 21:45:20 -0300

    Add reactivity data to component level

--- imports/ui/components/OptionsModal.jsx
+++ imports/ui/components/OptionsModal.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Meteor } from 'meteor/meteor';
+import { createContainer } from 'meteor/react-meteor-data';
 import Login from './Login';
 import Signup from './SignUp';
 import ShoppingCartButton from './ShoppingCartButton';
 import OrdersButton from './OrdersButton';
 
 export default class OptionsModal extends Component {
-
+  //cd275555
   logout() {
     Meteor.logout((error) => {
       if (error) {
@@ -21,46 +22,31 @@ export default class OptionsModal extends Component {
       width: "100%",
       height: "100%"
     };
-    if (Meteor.userId()) {
+    let { currentUser } = this.props;
       return (
         <div id="optionsModal" className="modal" style={modalStyle}>
           <div className="modal-content">
             <div className="row">
               <div className="col s12 m6">
-                <ShoppingCartButton/>
+                {currentUser ? <ShoppingCartButton/> : <Login/>}
               </div>
               <div className="col s12 m6">
-                <OrdersButton/>
+                {currentUser ? <OrdersButton/> : <Signup/>}
               </div>
             </div>
           </div>
           <div className="modal-footer">
             <a href="" className=" modal-action modal-close waves-effect waves-green btn left">Fechar</a>
-            <a href="" className="modal-action waves-effect waves-red btn right" onClick={this.logout}>Logout</a>
-          </div>
-        </div>
-      );
-    }
-      return (
-        <div id="optionsModal" className="modal" style={modalStyle}>
-          <div className="modal-content">
-            <div className="row">
-              <div className="col s12 m6">
-                <div className="card-panel white">
-                  <Login/>
-                </div>
-              </div>
-              <div className="col s12 m6">
-                <div className="card-panel white">
-                  <Signup/>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="modal-footer">
-            <a href="" className="modal-action modal-close waves-effect waves-green btn left">Fechar</a>
+            {currentUser ? <a href="" className="modal-action waves-effect waves-red btn right" onClick={this.logout}>Logout</a> : ''}
           </div>
         </div>
       );
   }
 }
+
+export default OptionsModalContainer = createContainer( () => {
+  let currentUserId = Meteor.userId();
+  return {
+    currentUser: currentUserId,
+  }
+}, OptionsModal);

c5c7034707c1ca77f6bf7b9cffdf62f8a32fa1bc
andredeloliveira <oliveira.andrede@gmail.com>
2016-09-12 16:49:18 -0300

    Add new UI components

--- imports/ui/components/ProductItem.jsx
+++ imports/ui/components/ProductItem.jsx
@@ -27,6 +27,9 @@ export default class ProductItem extends Component {
       marginLeft: "45%",
       paddingBottom: "45px"
     };
+    const comprarStyle = {
+      margin: "20px"
+    }
     const textStyle = {
       fontSize: "2em",
       fontFamily: "'Raleway', sans-serif"
@@ -36,27 +39,100 @@ export default class ProductItem extends Component {
       fontFamily: "'Raleway', sans-serif",
       color: "green"
     }
+    const titleStyle = {
+      fontFamily: "'Raleway', sans-serif",
+    }
+
+    const mobileStyle = {
+      fontSize: "3.5em",
+    }
+
+    const googlePlayStyle = {
+      height: "90px",
+      width:  "90px",
+      margin: "20px"
+    }
+
+
     const { product } =  this.props ;
 
     return (
-      <div className="row">
-        <div className="col l6 m12 s12">
-            <CardBoardS />
-          <span style={textStyle}>
-            Experimente realidade virtual de uma maneira divertida e acessível com o seu celular!
-          </span>
-          <div className="center">
-            <div className="price-tag">
-              <span style={priceStyle}>
-                R$50,00
-              </span>
+      <div>
+        <div className="row">
+          <div className="col l6 m12 s12">
+            <div className="center">
+                <CardBoardS />
             </div>
-             <button className="waves-effect waves-light btn-large" onClick={this.buyItem.bind(this, product._id)}>Comprar</button>
+            <span style={textStyle}>
+              Experimente realidade virtual de uma maneira divertida e acessível com o seu celular!
+            </span>
+            <div className="center">
+              <div className="price-tag">
+                <span style={priceStyle}>
+                  Compre agora por apenas R$50,00
+                </span>
+              </div>
+               <button style={comprarStyle} className="waves-effect waves-light btn-large" onClick={this.buyItem.bind(this, product._id)}>Comprar</button>
+            </div>
+          </div>
+          <div className="col l6 m12 s12" >
+            <SlideShow />
           </div>
         </div>
-        <div className="col l6 m12 s12" >
-          <SlideShow />
+        <div className="grey lighten-2 container">
+          <hr />
         </div>
+        <div className="row">
+            <div className="col l12 m12 s12 center">
+            <h3 style={titleStyle}> Especificações</h3>
+            <i style={mobileStyle} className="fa fa-mobile"></i>
+              <br />
+              <ul style={textStyle}>
+                <li>
+                  - Se encaixa em celulares de até 6"
+                </li>
+                <li>
+                  - Funciona com iPhones e dispositivos Android.
+                </li>
+                <li>
+                  - Lentes biconvexas de 37 milímetros de diâmetro, com 50 milímetros Distância Focal.
+                </li>
+                <li>
+                  - Novo botão condutor substitui o interruptor magnético da versão 1. funciona tanto em iOS e dispositivos Android.
+                </li>
+                <li>
+                  - Inspirado pelo Google Cardboard v2.
+                </li>
+              </ul>
+              <div className="center">
+                <div className="price-tag">
+                  <span style={priceStyle}>
+                    Compre agora por apenas R$50,00
+                  </span>
+                </div>
+                 <button style={comprarStyle} className="waves-effect waves-light btn-large" onClick={this.buyItem.bind(this, product._id)}>Comprar</button>
+              </div>
+            </div>
+          </div>
+          <div className="grey lighten-2 container">
+            <hr />
+          </div>
+          <div className="row">
+            <div className="center">
+              <h3 style={titleStyle}>Seu cardboard não chegou ainda?</h3>
+              <span style={textStyle}>
+                Baixe o HologramAR e veja incríveis hologramas na sua frente
+              </span>
+              <div className="center">
+                <a href="https://play.google.com/store/apps/details?id=com.AIS.HologramARcas">
+                  <img style={googlePlayStyle} src="https://s3-sa-east-1.amazonaws.com/aisambientesdigitais/geral/google-play.png"
+                    alt="Google Play Link" />
+                </a>
+                <br />
+                <span className={textStyle}>HologramAR</span>
+              </div>
+            </div>
+          </div>
       </div>
     );
   }
--- imports/ui/components/SlideShow.jsx
+++ imports/ui/components/SlideShow.jsx
@@ -10,12 +10,16 @@ export default class SlideShow extends Component {
   }
   render() {
     const slideShowStyle = {
-      height: "auto",
+      maxHeight: "100%",
+      minHeight: "400px",
+      borderRadius: "10px"
     }
     return (
       <div className="carousel carousel-slider">
-        <a className="carousel-item" href="#one!"><img src="https://s3-sa-east-1.amazonaws.com/aisambientesdigitais/geral/garota-cardboard.jpg" style={slideShowStyle} /></a>
-        <a className="carousel-item" href="#two!"><img src="https://s3-sa-east-1.amazonaws.com/aisambientesdigitais/geral/garota-cardboard.jpg" style={slideShowStyle} /></a>
+        <a className="carousel-item" href=""><img src="https://s3-sa-east-1.amazonaws.com/aisambientesdigitais/geral/card-frente.png" style={slideShowStyle} /></a>
+        <a className="carousel-item" href=""><img src="https://s3-sa-east-1.amazonaws.com/aisambientesdigitais/geral/card-front.png" style={slideShowStyle} /></a>
+        <a className="carousel-item" href=""><img src="https://s3-sa-east-1.amazonaws.com/aisambientesdigitais/geral/card-lateral1.png" style={slideShowStyle} /></a>
+        <a className="carousel-item" href=""><img src="https://s3-sa-east-1.amazonaws.com/aisambientesdigitais/geral/card-lateral2.png" style={slideShowStyle} /></a>
       </div>
     )
   }
